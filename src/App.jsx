68d1c963e6499d682b66f28ebe8519663a606037
daiterranova <daiana.terranova@gmail.com>
2023-01-30 19:38:03 -0300

    init project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <main>
+      <h1>Meals app</h1>
+    </main>
+  );
+}
+
+export default App;
