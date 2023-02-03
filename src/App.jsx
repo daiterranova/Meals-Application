81bad8b984928a2597a0556a74be5a0d62844008
daiterranova <daiana.terranova@gmail.com>
2023-02-03 15:20:57 -0300

    comment unused styles on index.css and components without changes on App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,10 @@ import "./App.css";
 function App() {
   return (
     <main>
-      <Favorites />
+      {/*  <Favorites /> */}
       <Meals />
-      <Modal />
-      <Search />
+      {/* <Modal />
+      <Search /> */}
     </main>
   );
 }
