50eb1d32bbd5ba59a79b5d8158c6493258befad7
daiterranova <daiana.terranova@gmail.com>
2023-02-11 18:33:04 -0300

    change Search component structure

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,34 @@
+import { useState } from "react";
+import { useGlobalContext } from "../context";
+
 const Search = () => {
-  return <h1> Search</h1>;
+  const [text, setText] = useState("");
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <header className="header search-container">
+      <form onSubmit={handleSubmit}>
+        <input
+          className="form-input"
+          type="text"
+          value={text}
+          placeholder="type your favorite meal"
+          onChange={handleChange}
+        />
+        <button className="btn" type="submit">
+          search
+        </button>
+        <button className="btn btn-hipster" type="button">
+          surprise me!
+        </button>
+      </form>
+    </header>
+  );
 };
 export default Search;
