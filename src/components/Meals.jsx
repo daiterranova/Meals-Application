cd6f2409f4e319d7404c793898ef1705425bfa5a
daiterranova <daiana.terranova@gmail.com>
2023-02-15 16:41:34 -0300

    add html structure and styles

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -25,7 +25,7 @@ const Meals = () => {
         const { idMeal, strMeal: title, strMealThumb: image } = meal;
         return (
           <article key={idMeal} className="single-meal">
-                <img src={image} className="img" onClick={() => {selectMeal(idMeal)}}/>
+            <img src={image} className="img" onClick={() => {selectMeal(idMeal)}}/>
             <footer>
               <h5>{title}</h5>
               <button className="like-btn">
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -3,12 +3,22 @@ import { useGlobalContext } from "../context";
 const Modal = () => {
   const {selectedMeal, closeModal} = useGlobalContext();
 
+  const {strMeal: title, strMealThumb: image, strSource: source, strInstructions: description, strCategory: tag } = selectedMeal;
   return (
     <aside className="modal-overlay">
-      <div className="modal-container">
-        <h1>Modal Component</h1>
-        <button onClick={closeModal}>x</button>
-      </div>
+      <section className="modal-container">
+        <img src={image} className="img modal-img" />
+        <article className="modal-content">
+          <h4>{title}</h4>
+          <p>Instructions:</p>
+          <p>{description}</p>
+          <div>
+            <a href={source} target="_blank">Original Source</a>
+            <p>{tag}</p>
+          </div>
+          <button className="close-btn btn btn-hipster" onClick={closeModal}>close</button>
+        </article>
+      </section>
     </aside>
   )
   
