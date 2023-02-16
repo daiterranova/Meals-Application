5e5a80d72ec69bc46d2d84044c48d946c81637c6
daiterranova <daiana.terranova@gmail.com>
2023-02-16 14:48:59 -0300

    implement html structure on Favorites component and render

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -1,4 +1,26 @@
+import { useGlobalContext } from "../context";
+
 const Favorites = () => {
-  return <h1> Vamo arriba</h1>;
+
+const {selectMeal, favorites,removeFromFavorites } = useGlobalContext();
+
+  return (
+    <section className="favorites">
+      <div className="favorites-content">
+        <h5>Favorites</h5>
+        <div className="favorites-container">
+          {
+            favorites.map((favorite) => {
+              const {idMeal,strMealThumb: image,strMeal:title } = favorite
+              return (  <div className="favorite-item img">
+                  <img id={idMeal} src={image} alt={title} className="favorites-img img"/>
+                  <button className="remove-btn" onClick={(idMeal) => removeFromFavorites(idMeal) }>remove</button>
+                </div>
+            )})
+          }
+        </div>
+      </div>
+    </section>
+  );
 };
 export default Favorites;
