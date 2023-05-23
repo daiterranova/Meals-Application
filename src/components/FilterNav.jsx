975ae637c76b741e0a9aeb7ab6f28bc0018a846e
daiterranova <daiana.terranova@gmail.com>
2023-05-23 19:37:00 -0300

    create html structure of navbar using bootstrap template

--- src/components/FilterNav.jsx
+++ src/components/FilterNav.jsx
@@ -1,13 +1,35 @@
 const FilterNav = () => {
     return (
-        <>
-            <nav>
-                <a class="active" href="#home">All categories</a>
-                <a href="#news">Side</a>
-                <a href="#contact">Vegetarian</a>
-                <a href="#about">Dessert</a>
+
+        <div>
+            <nav className="navbar navbar-expand-lg bg-light">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">Filter</a>
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarNavAltMarkup"
+                        aria-controls="navbarNavAltMarkup"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div className="navbar-nav">
+                            <a className="nav-link active" aria-current="page" href="#"
+                            >All categories</a
+                            >
+                            <a className="nav-link" href="#">Side</a>
+                            <a className="nav-link" href="#">Vegetarian</a>
+                            <a className="nav-link">Dessert</a>
+                        </div>
+                    </div>
+                </div>
             </nav>
-        </>
+        </div>
+
     )
 }
 export default FilterNav;
