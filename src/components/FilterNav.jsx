43b5250e5e19a013e60f8f8124e0d1ea64113381
daiterranova <daiana.terranova@gmail.com>
2023-05-25 16:31:38 -0300

    implement filterMealsByCategory when click a category link to filter meals
    (cherry picked from commit 837b148f6761032e1ed5a28a625190ec2a9b3d69)
    Signed-off-by: daiterranova <daiana.terranova@gmail.com>

--- src/components/FilterNav.jsx
+++ src/components/FilterNav.jsx
@@ -1,24 +1,35 @@
+import { useGlobalContext } from "../context";
+
 const FilterNav = () => {
+    const { filterMealsByCategory } = useGlobalContext();
+    const handleClick = (e, cat) => {
+        e.preventDefault();
+        filterMealsByCategory(cat)
+    }
+
     return (
 
         <section className="navbar-container bg-light">
             <ul className="nav justify-content-center">
-                <li className="nav-item dropdown text-secondary active">
-                    <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">All</a>
+                <li className="nav-item">
+                    <a className="nav-link text-secondary fw-bold" id="dessert" href="#" onClick={e => handleClick(e, 'All')}>All</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link text-secondary" id="side" href="#" onClick={e => handleClick(e, 'Side')}>Side</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link text-secondary" id='vegetarian' onClick={e => handleClick(e, 'Vegetarian')} href="#">Vegetarian</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link text-secondary" id="dessert" href="#" onClick={e => handleClick(e, 'Dessert')}>Dessert</a>
+                </li>
+                <li className="nav-item dropdown">
+                    <a className="nav-link dropdown-toggle text-secondary" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Others</a>
                     <ul className="dropdown-menu">
                         <li><a className="dropdown-item" href="#">category1</a></li>
                         <li><a className="dropdown-item" href="#">category2</a></li>
                         <li><a className="dropdown-item" href="#">category3</a></li>
                     </ul>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link text-secondary" href="#">Side</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link text-secondary" href="#">Vegetarian</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link text-secondary">Dessert</a>
                 </li>
             </ul>
         </section >
